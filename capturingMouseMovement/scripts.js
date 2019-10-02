6ba3a301f262de875cbc882f65f513ffca3d6195
April Copes <antdevelopment1@gmail.com>
2019-10-02 01:09:46 -0400

    add functionality for eyes to do the inverse of mouse movement as an output

--- capturingMouseMovement/scripts.js
+++ capturingMouseMovement/scripts.js
@@ -15,19 +15,26 @@ let input = {
     height: 0
   }
 };
+
 input.mouseX.range = input.mouseX.end - input.mouseX.start;
 input.mouseY.range = input.mouseY.end - input.mouseY.start;
 
 // Output Setup
 var output = {
   x: {
-    start: -100,
-    end: 100,
+    start: -70,
+    end: 70,
     current: 0
   },
-  y: {}
+  y: {
+	  start: -75,
+	  end: 75,
+	  current: 0
+  }
 };
+
 output.x.range = output.x.end - output.x.start;
+output.y.range = output.y.end - output.y.start;
 
 // Fraction Value Code
 let handleMouseMove = event => {
@@ -41,12 +48,22 @@ let handleMouseMove = event => {
 	input.mouseY.fraction =
 		(input.mouseY.current - input.mouseY.start) / input.mouseY.range;
 
-  	// Output X
-  	output.x.current = output.x.start + input.mouseX.fraction * output.x.range;
+  	// Mouse Output X 
+  	output.x.current = output.x.end - input.mouseX.fraction * output.x.range;
+
+	// Mouse Output Y
+	output.y.current = output.y.end - input.mouseY.fraction * output.y.range;
+
+	// To make eyeballs go in the SAME DIRECTION as the mouse you can do...
+	// output.y.current = output.y.start + input.mouseY.fraction * output.y.range;
+	// output.x.current = output.x.start + input.mouseX.fraction * output.x.range;
+
+
 	pupilsArray.forEach(function (pupil, i) {
-		pupil.style.transform = 'translateX(' + output.x.current + 'px)';
+		pupil.style.transform = 'translate(' + output.x.current + 'px,' + output.y.current + 'px)';
 	})
 
+	console.log(output.y.current, "This is th e outp")
 	console.log("output.x.current", output.x.current);
 	// console.log("fraction mouse value for X", input.mouseX.fraction);
 	// console.log("fraction mouse value for Y", input.mouseY.fraction);
