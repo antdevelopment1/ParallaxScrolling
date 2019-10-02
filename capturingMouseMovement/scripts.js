bff82f81c06ff18617e031dff3a5543642d6431a
April Copes <antdevelopment1@gmail.com>
2019-10-02 00:36:45 -0400

    adds code for output to control eyes

--- capturingMouseMovement/scripts.js
+++ capturingMouseMovement/scripts.js
@@ -1,55 +1,66 @@
+// HTML Setup
+let pupilsHTMLCollection = document.getElementsByClassName("pupil");
+let pupilsArray = Array.from(pupilsHTMLCollection);
+
 // Input Setup
 let input = {
-    mouseX: {
-        start: 0,
-        end: window.innerWidth,
-        current: 0
-    },
-    mouseY: {
-        start: 0,
-        end: window.innerHeight,
-        height: 0
-    }
-}
+  mouseX: {
+    start: 0,
+    end: window.innerWidth,
+    current: 0
+  },
+  mouseY: {
+    start: 0,
+    end: window.innerHeight,
+    height: 0
+  }
+};
 input.mouseX.range = input.mouseX.end - input.mouseX.start;
 input.mouseY.range = input.mouseY.end - input.mouseY.start;
 
 // Output Setup
 var output = {
-    x: {
-        start: -100,
-        end: 100,
-        current: 0
-    },
-    y: {}
-}
+  x: {
+    start: -100,
+    end: 100,
+    current: 0
+  },
+  y: {}
+};
 output.x.range = output.x.end - output.x.start;
 
 // Fraction Value Code
-let handleMouseMove = (event) => {
-    // Mouse x input
-    input.mouseX.current = event.clientX;
-    input.mouseX.fraction = (input.mouseX.current - input.mouseX.start) / input.mouseX.range;
+let handleMouseMove = event => {
+  	// Mouse x input
+	input.mouseX.current = event.clientX;
+	input.mouseX.fraction =
+		(input.mouseX.current - input.mouseX.start) / input.mouseX.range;
 
-    // Mouse Y Input
-    input.mouseY.current = event.clientY;
-    input.mouseY.fraction = (input.mouseY.current - input.mouseY.start) / input.mouseY.range;
-    
-    output.x.current = output.x.start + (input.mouseX.fraction * output.x.range);
-    console.log('output.x.current', output.x.current);
-    // console.log("fraction mouse value for X", input.mouseX.fraction);
-    // console.log("fraction mouse value for Y", input.mouseY.fraction);
-}
+  	// Mouse Y Input
+	input.mouseY.current = event.clientY;
+	input.mouseY.fraction =
+		(input.mouseY.current - input.mouseY.start) / input.mouseY.range;
+
+  	// Output X
+  	output.x.current = output.x.start + input.mouseX.fraction * output.x.range;
+	pupilsArray.forEach(function (pupil, i) {
+		pupil.style.transform = 'translateX(' + output.x.current + 'px)';
+	})
+
+	console.log("output.x.current", output.x.current);
+	// console.log("fraction mouse value for X", input.mouseX.fraction);
+	// console.log("fraction mouse value for Y", input.mouseY.fraction);
+};
 
 let handleResize = () => {
-    input.mouseX.end = window.innerWidth;
-    input.mouseX.range = input.mouseX.end - input.mouseX.start;
+	input.mouseX.end = window.innerWidth;
+	input.mouseX.range = input.mouseX.end - input.mouseX.start;
 
-    input.mouseY.end = window.innerHeight;
-    input.mouseY.range = input.mouseY.end - input.mouseY.start;
-}
+	input.mouseY.end = window.innerHeight;
+	input.mouseY.range = input.mouseY.end - input.mouseY.start;
+};
 // Event Listener to trigger mouse mousemovement
-window.addEventListener('mousemove', handleMouseMove);
+window.addEventListener("mousemove", handleMouseMove);
 
 // Event handeler that handles screen resize should the window's width get wider or smaller it will recalculate the range
-window.addEventListener('resize', handleResize);
+window.addEventListener("resize", handleResize);
